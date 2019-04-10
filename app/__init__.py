66f00d96fa786601ed2dd9af667b5e9a5d460163
NekoNikon <nikon9nikon@gmail.com>
2019-04-11 00:09:09 +0600

    architecture

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,42 @@
-from flask import Flask 
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask import Flask, request, current_app
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
+from elasticsearch import Elasticsearch
+import rq
 from config import Config
 
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+mail = Mail()
+bootstrap = Bootstrap()
+moment = Moment()
+babel = Babel()
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app)
 
-from app import views 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+from app.auth import views
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!rate_system/bin/python3
 
-from app import app
+from app.main import app
 
 if __name__ == "__main__":
     app.run(debug = True , host='0.0.0.0')
